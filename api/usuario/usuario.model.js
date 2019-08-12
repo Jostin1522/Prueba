f8e4f873fd912c43b664861830ef18654316a4c5
Jostin <jalvaradoc2@ucenfotec.ac.cr>
2019-08-12 09:26:06 -0600

    hj

--- api/usuario/usuario.model.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var usuarioSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId, //String único aleatorio
-  correo: String,
-  libreria: String
-});
-
-module.exports = mongoose.model('Usuario', usuarioSchema,'Usuarios');
